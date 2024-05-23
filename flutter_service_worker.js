048cd75a31342584a8f373ff97589d5ff7f3b6b3
josterga <josterga@uci.edu>
2024-05-22 22:54:40 -0700

    ux tweaks

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "e789050279dae23c0884dff9bc00ce0d",
+"flutter_bootstrap.js": "78aa5be9a644875bc4c444dbbcfdffe1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fd5f7f6d7e95ced50289cca40dc008c9",
-"/": "fd5f7f6d7e95ced50289cca40dc008c9",
-"main.dart.js": "31e458352c9fec1d198c7b41bb6d7cdf",
+"index.html": "2d72186271d3e73ce7ebf523ddc1fe36",
+"/": "2d72186271d3e73ce7ebf523ddc1fe36",
+"main.dart.js": "8567d66bbc28d5c0cb0ff145981d1e91",
 "manifest.json": "cea689f6e8f1a848b92980c319b2398d",
 "version.json": "adbc6c71f6446c8841399c12a5414f43"};
 // The application shell files that are downloaded before a service worker can
